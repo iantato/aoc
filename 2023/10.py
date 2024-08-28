499867692221d0d964b2980b5baa4ef532367d4f
iantato <83338977+iantato@users.noreply.github.com>
2024-08-29 01:18:43 +0800

    D10: Finished Part 2

--- 2023/10.py
+++ 2023/10.py
@@ -8,7 +8,7 @@ https://adventofcode.com/2023/day/10
 '''
 
 import os
-from collections import deque
+from collections import deque, defaultdict
 
 class PipeMaze():
 
@@ -74,7 +74,7 @@ class PipeMaze():
     # This is the main function that counts the steps of the
     # traversal of the loop. The steps is then divided by
     # 2 so that we can get the final answer for the problem.
-    def count_steps(self) -> None:
+    def count_steps(self) -> int:
         steps = 1
 
         x, y = self.find_start_coords()
@@ -88,3 +88,69 @@ class PipeMaze():
             self.check_connections(x, y, neighbors, visited)
 
         return steps // 2
+
+    # This changes the starting tile to a proper pipe as the
+    # old value of the starting position is 'S'. This is required
+    # for the counting of the tiles that are inside the loop.
+    def change_start(self) -> None:
+        x, y = self.find_start_coords()
+        neighbors = self.find_start_neighbors(x, y)
+        start_offsets = []
+
+        for cx, cy in neighbors:
+            start_offsets.append((cx - x, cy - y))
+        for pipe, offset in self.PIPES_OFFSET.items():
+            # We use this scuffed if-statement because we need to check
+            # what the pipe is with the offset, but the problem is that
+            # my start_offset can have different positions than the
+            # constant dictionary PIPES_OFFSET,
+            if tuple(start_offsets) == (offset[0], offset[1]) or tuple(start_offsets) == (offset[1], [0]):
+                self.input[y][x] = pipe
+
+    # We can count the tiles inside of the loop by using the Ray-tracing algorithm or
+    # also known as the Even-Odd Rule algorithm. We count how many sides we have passed
+    # by and check if it's odd or even. If it's odd, that means the tile is inside and
+    # if it's even, that means the tile is outside. We don't count '-' as it is collinear
+    # with the pipes that we have passed through; we also don't count F--7 and L--J as
+    # they don't vertically cross the row, they go to different vertical rows.
+    def count_inside(self) -> int:
+        count = 0
+        pipes = defaultdict(lambda: (140, 0))
+
+        x, y = self.find_start_coords()
+        neighbors = self.find_start_neighbors(x, y)
+        visited = set({(x, y)})
+        self.change_start()
+
+        while len(neighbors) > 0:
+            x, y = neighbors.pop()
+            visited.add((x, y))
+            pipes[y] = (min(pipes[y][0], x), max(pipes[y][1], x))
+            self.check_connections(x, y, neighbors, visited)
+
+        for y, x_range in pipes.items():
+            cross = 0
+            start = ''
+            for x in range(x_range[0], x_range[1]):
+                tile = self.input[y][x]
+                if (x, y) not in visited:
+                    if cross % 2 == 1:
+                        count += 1
+                else:
+                    match tile:
+                        case 'F':
+                            start = 'F'
+                        case 'L':
+                            start = 'L'
+                        case '|':
+                            cross += 1
+                        case 'J':
+                            if start == 'F':
+                                cross += 1
+                                start = ''
+                        case '7':
+                            if start == 'L':
+                                cross += 1
+                                start = ''
+
+        return count
