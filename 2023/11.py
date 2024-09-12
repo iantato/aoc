58c8d7ccd40fea59d378a2650a893159475ccab6
iantato <83338977+iantato@users.noreply.github.com>
2024-09-13 01:47:13 +0800

    D11: Refactored for Part 2

--- 2023/11.py
+++ 2023/11.py
@@ -7,53 +7,40 @@ https://adventofcode.com/2023/day/11
 
 class CosmicExpansion:
 
+    # Multiplier used for how much you want to expand the universe.
+    # Keep in mind to always -1 it if multiplier != 1 because of the
+    # extra character we have in each expansion.
+    MULTIPLIER = 1000000 - 1
+
     def __init__(self, data: str) -> None:
         self.input = open(data).read().splitlines()
 
-    # Expand the universe by duplicating and checking whether
-    # a whole row does not have any galaxies ('#'). We then
-    # check if a whole column does not have any galaxies.
-    def expand_universe(self) -> list[str]:
-        universe = list([])
-
-        for row in self.input:
-            if '#' not in row:
-                universe.append(row)
-            universe.append(row)
-
-        idx = 0
-        while idx != len(universe[0]):
-            if '#' not in [i[idx] for i in universe]:
-                for i in range(len(universe)):
-                    universe[i] = universe[i][:idx] + '.' + universe[i][idx:]
-                idx += 1
-            idx += 1
-
-        return universe
-
-    # Get the galaxy positions by checking where the
-    # '#' are and coordinating them to (y, x).
-    def galaxy_position(self) -> list[tuple[int, int]]:
-        universe = self.expand_universe()
+    # Get all the galaxy positions and store them in (y, x) format.
+    def galaxy_positions(self) -> list[tuple[int, int]]:
         galaxies = list([])
 
-        for y, row in enumerate(universe):
+        for y, row in enumerate(self.input):
             for x, col in enumerate(row):
                 if col == '#':
                     galaxies.append((y, x))
 
         return galaxies
 
-    # Get the shortest path of each galaxies by
-    # subtracting the coordinates to each other.
-    # We then use abs() to make negative numbers into
-    # positive numbers.
+    # Find the shortest path between all galaxies pair.
     def find_path_total(self) -> None:
         total = 0
-        galaxies = self.galaxy_position()
+        galaxies = self.galaxy_positions()
+
+        # Get coordinates of the rows and columns that needs to be expanded so
+        # that we can use it in the calculation later. This is so that we don't
+        # have to brute-force the expansion and instead can just use calculations
+        # by finding the position of the expansion and the galaxies.
+        expanded_rows = [x for x, i in enumerate(self.input) if '#' not in i]
+        expanded_col = [x for x, i in enumerate(self.input) if '#' not in [i[x] for i in self.input]]
 
         for y, origin in enumerate(galaxies):
             for galaxy in galaxies[y+1:]:
-                total += abs(origin[0] - galaxy[0]) + abs(origin[1] - galaxy[1])
+                total += abs(origin[0] - galaxy[0]) + (len([i for i in expanded_rows if i >= min(origin[0], galaxy[0]) and i <= max(origin[0], galaxy[0])]) * self.MULTIPLIER)
+                total += abs(origin[1] - galaxy[1]) + (len([i for i in expanded_col if i >= min(origin[1], galaxy[1]) and i <= max(origin[1], galaxy[1])]) * self.MULTIPLIER)
 
         return total
